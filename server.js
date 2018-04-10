226b39cd78200625c1d90edfb66a8b7810b9c06b
aleks <aleksnyombi@gmail.com>
2018-04-10 16:41:44 +0300

    first thigs are confusing

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const fs = require('fs');
+
+
+app.use(express.static(__dirname +'/public'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('capitizer', (text) => {
+    return text.toUpperCase();
+});
+
+app.use((request, response, next) => {
+    let msg = `url -> ${request.url} and protocal -> ${request.protocol}\n`;
+    fs.appendFile('server.log', msg, ($err)=> {
+        console.log($err);
+    })
+    next();
+});
+
+// app.use((request, response, next)=> {
+//     response.render('mentenance.hbs');
+// });
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        title: 'home',
+        user: {
+            username: 'aleks',
+            age: 35
+        }
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        title: 'ABOUT',
+        message: 'Welcome home buddy'
+    });
+});
+
+app.get('/bad', (request, response) => {
+    response.send({
+        error: "you are fire mr salimo"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("server running on port 3000");
+});
