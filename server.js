e18e2c51e21dc24cc4dc6df695321d4a57f124c2
aleks <aleksnyombi@gmail.com>
2018-04-10 18:09:58 +0300

    another one

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const hbs = require('hbs');
 const app = express();
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 
 app.use(express.static(__dirname +'/public'));
 app.set('view engine', 'hbs');
@@ -50,6 +52,6 @@ app.get('/bad', (request, response) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("server running on port 3000");
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
 });
